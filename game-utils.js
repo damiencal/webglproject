1669017df90b071dcfe2fa58329a94834cd60de5
damiencal <damien@calvignac.com>
2015-05-21 16:06:46 +0200

    Added/Iniciated sounds

--- game-utils.js
+++ game-utils.js
@@ -119,3 +119,69 @@ function mvPopMatrix() {
 function degToRad(degrees) {
         return degrees * Math.PI / 180;
     }
+
+/*
+
+    Sounds
+
+    The sounds class is used to asynchronously load sounds and allow
+    them to be played.
+
+*/
+function Sounds() {
+
+    //  The audio context.
+    this.audioContext = null;
+
+    //  The actual set of loaded sounds.
+    this.sounds = {};
+}
+
+Sounds.prototype.init = function() {
+
+    //  Create the audio context, paying attention to webkit browsers.
+    context = window.AudioContext || window.webkitAudioContext;
+    this.audioContext = new context();
+    this.mute = false;
+};
+
+Sounds.prototype.loadSound = function(name, url) {
+
+    //  Reference to ourselves for closures.
+    var self = this;
+
+    //  Create an entry in the sounds object.
+    this.sounds[name] = null;
+
+    //  Create an asynchronous request for the sound.
+    var req = new XMLHttpRequest();
+    req.open('GET', url, true);
+    req.responseType = 'arraybuffer';
+    req.onload = function() {
+        self.audioContext.decodeAudioData(req.response, function(buffer) {
+            self.sounds[name] = {buffer: buffer};
+        });
+    };
+    try {
+      req.send();
+    } catch(e) {
+      console.log("An exception occured getting sound the sound " + name + " this might be " +
+         "because the page is running from the file system, not a webserver.");
+      console.log(e);
+    }
+};
+
+Sounds.prototype.playSound = function(name) {
+
+    //  If we've not got the sound, don't bother playing it.
+    if(this.sounds[name] === undefined || this.sounds[name] === null || this.mute === true) {
+        return;
+    }
+
+    //  Create a sound source, set the buffer, connect to the speakers and
+    //  play the sound.
+    var source = this.audioContext.createBufferSource();
+    source.buffer = this.sounds[name].buffer;
+    source.connect(this.audioContext.destination);
+    source.start(0);
+};
