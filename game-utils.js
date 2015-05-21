753969f245f9d9dba169713f32c61126d7f53977
damiencal <damien@calvignac.com>
2015-05-21 16:26:21 +0200

    added collision handler

--- game-utils.js
+++ game-utils.js
@@ -128,6 +128,20 @@ function degToRad(degrees) {
     them to be played.
 
 */
+//  Mutes or unmutes the game.
+mute = function(mute) {
+
+    //  If we've been told to mute, mute.
+    if(mute === true) {
+        this.sounds.mute = true;
+    } else if (mute === false) {
+        this.sounds.mute = false;
+    } else {
+        // Toggle mute instead...
+        this.sounds.mute = this.sounds.mute ? false : true;
+    }
+};
+
 function Sounds() {
 
     //  The audio context.
@@ -185,3 +199,46 @@ Sounds.prototype.playSound = function(name) {
     source.connect(this.audioContext.destination);
     source.start(0);
 };
+
+
+/**
+ * Detect colisions between the lasers and enemies
+ */
+var detectColisions = function() {
+	for(indexLaser in lasers) {
+		for(indexEnemy in enemies) {
+			if(colisionHandler.detectColisionBetweenObjects(lasers[indexLaser], enemies[indexEnemy])) {
+				lasers.splice(indexLaser, 1);
+				enemies.splice(indexEnemy, 1);
+				break;
+			}
+		}
+	}
+}
+
+/**
+  * Class ColisionHandler
+  */
+ function ColisionHandler() {
+
+ 	/**
+ 	 * Checks the colision between two objects
+ 	 * @param obj1 - first object
+ 	 * @param obj2 - second object
+ 	 */
+ 	this.detectColisionBetweenObjects = function(obj1, obj2) {
+ 		if((obj1.getPositionY() + obj1.height) < obj2.getPositionY()) {
+ 			return false;
+ 		}
+ 		else if(obj1.getPositionY() > (obj2.getPositionY() + obj2.height)) {
+ 			return false;
+ 		}
+ 		else if((obj1.getPositionX() + obj1.width) < obj2.getPositionX()) {
+ 			return false;
+ 		}
+ 		else if(obj1.getPositionX() > (obj2.getPositionX() + obj2.width)) {
+ 			return false;
+ 		}
+ 		return true;
+ 	}
+ }
